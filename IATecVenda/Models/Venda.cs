57226a29b2c4d359e4c7647795698e2b513bb869
Anderson Oliveira <anderson.l.o@goulook.com>
2024-11-30 21:32:36 -0300

    atualizado a estrutura das pastas

---
+++ IATecVenda/Models/Venda.cs
@@ -0,0 +1,12 @@
+using IATecVenda.Enums;
+    
+namespace IATecVenda.Models;
+
+public class Venda
+{
+    public int Id { get; set; }
+    public Vendedor Vendedor { get; set; }
+    public List<Item> Itens { get; set; }    
+    public DateTime DataVenda { get; set; }
+    public StatusVenda Status { get; set; }
+}
--- Models/Venda.cs
+++
@@ -1,7 +0,0 @@
-public class Venda
-{
-    public int Id { get; set; }
-    public Vendedor Vendedor { get; set; }
-    public int List<Item> Itens { get; set; }    
-    public DateTime DataVenda { get; set; }
-}
